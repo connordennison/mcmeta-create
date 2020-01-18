5f96d7864dec8bea906e4ee31998692a0cd4aee9
Connor <connor@dennison.email>
2020-01-18 12:29:05 +0000

    Update pack creator.py

--- pack creator.py
+++ pack creator.py
@@ -4,7 +4,7 @@ from tkinter import *
 
 
 window = Tk()
-window.title("pack.mcmeta Creator v1.2")
+window.title("pack.mcmeta Creator v1.3")
 window.configure(bg="black")
 window.resizable(width=FALSE, height=FALSE)
 
@@ -29,7 +29,7 @@ def generatemcmeta():
 # tkinter elements
 
 titleLabel = Label(window, text="pack.mcmeta Creator", bg="black", fg="white", font=("none", 25)) .pack()
-titleSpace = Label(window, text="by ThreeSpy712", bg="black", fg="white", font=("none", 7)) .pack()
+titleSpace = Label(window, text="by cnnd", bg="black", fg="white", font=("none", 7)) .pack()
 versionLabel = Label(window, text="Please enter the desired Minecraft version below.", bg="black", fg="white") .pack()
 versionEntry = Entry(window, bg="white", fg="black")
 versionEntry.pack()
@@ -37,6 +37,7 @@ versionHelpLabel1 = Label(window, text="1 for Minecraft 1.6 to 1.8", bg="black",
 versionHelpLabel2 = Label(window, text="2 for Minecraft 1.9 and 1.10", bg="black", fg="white", font=("none", 10)) .pack()
 versionHelpLabel3 = Label(window, text="3 for Minecraft 1.11 and 1.12", bg="black", fg="white", font=("none", 10)) .pack()
 versionHelpLabel4 = Label(window, text="4 for Minecraft 1.13 and 1.14", bg="black", fg="white", font=("none", 10)) .pack()
+versionHelpLabel4 = Label(window, text="5 for Minecraft 1.15", bg="black", fg="white", font=("none", 10)) .pack()
 nameLabel = Label(window, text="Please enter the name of the texture pack below.", bg="black", fg="white") .pack()
 nameEntry = Entry(window, bg="white", fg="black")
 nameEntry.pack()
